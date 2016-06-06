1d3829c7d049fb924ff5544a4eb44f3ab6487d74
Love <Love@love.local>
2016-06-06 23:14:16 +0530

    Initial Commit

---
+++ TwitterPost/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwitterPost
+//
+//  Created by Love on 6/6/16.
+//  Copyright © 2016 Love. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwitterPost/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwitterPost
+//
+//  Created by Love on 6/6/16.
+//  Copyright © 2016 Love. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
